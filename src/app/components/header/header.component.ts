2145e1fe65354bc1b554a358db9d3eb1efb62c6d
Hithaesh <hithaeshpraveen2002@gmail.com>
2024-06-18 00:51:27 +0530

    added routing

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
@@ -10,12 +12,20 @@ import { Subscription } from 'rxjs';
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
   showAddTask: boolean | undefined = false;
-  subcription: Subscription | undefined;
+  subscription: Subscription | undefined;
 
-
-  constructor(private uiService: UiService) {
-    this.subcription = this.uiService.onToggle().subscribe((value) => {
-      this.showAddTask = value; // value = servicefile(showAddTask value opposite to it) // this.subject.next(this.showAddTask)
+//Todo: We need to handle the case, when Add button is clicked, the Add-Task Component should be visible.
+//! Using Dependency Injection
+  constructor(private uiService: UiService, private router: Router) {
+    //Todo: Setting up the subscription
+    // Toggling the button. Between ADD and CLOSE
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => {
+      this.showAddTask = value; 
+      // Observable is passed from the service file.
+      // value = servicefile(showAddTask value opposite to it) 
+      // this.subject.next(this.showAddTask)
     });
    }
 
@@ -28,5 +38,11 @@ export class HeaderComponent implements OnInit {
   //Todo: Change the showAddTask value
   toggleAddTask() {
     this.uiService.toggleAddTask();
+  }
+
+  //Todo: ADD/CLOSE Button will be visible only in the '/' Path.
+  hasRoute(route: string){
+    console.log(this.router.url);
+    return this.router.url === route;
   }
 }
